57d9fc1a67bb6b560e85a14ee6cbd1a679491bb6
mohd198847 <71812308+mohd198847@users.noreply.github.com>
2024-03-26 14:56:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(b'I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison 00')
+        self.wfile.write(b"I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison 00")
 
 httpd = socketserver.TCPServer(('', 5000), Handler)
 httpd.serve_forever()
